f359eb40d1ba8f092f137ef88f457ce8c81eb94a
Kacper Aleks <259086@student.pwr.edu.pl>
2023-04-21 14:31:33 +0000

    Merged PR 41: Move profiles config to application.properties
    Move profiles config to application.properties

--- backend/src/main/java/com/kpz/codereview/CodeReviewApplication.java
+++ backend/src/main/java/com/kpz/codereview/CodeReviewApplication.java
@@ -1,6 +1,5 @@
 package com.kpz.codereview;
 
-import com.kpz.codereview.config.model.AppProfiles;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
@@ -10,11 +9,6 @@ import org.springframework.context.annotation.PropertySource;
 public class CodeReviewApplication {
 
 	public static void main(String[] args) {
-		SpringApplication application =
-				new SpringApplication(CodeReviewApplication.class);
-
-		//Current profiles = [DEV_PROFILE, TEST_PROFILE]
-		application.setAdditionalProfiles(AppProfiles.TEST_PROFILE.toString());
-		application.run(args);
+		SpringApplication.run(CodeReviewApplication.class, args);
 	}
 }
