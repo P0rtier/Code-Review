26ab99d8aa32e3b5bf6b54d13e403c2e1a791cae
Damian Gnieciak <259065@student.pwr.edu.pl>
2023-05-13 19:31:52 +0000

    Merged PR 85: Changed background color of reviews to assign
    Changed background color of reviews to assign
    
    Related work items: #99

--- frontend/src/pages/home/components/reviews-to-assign/components/ReviewToAssignComponent.tsx
+++ frontend/src/pages/home/components/reviews-to-assign/components/ReviewToAssignComponent.tsx
@@ -3,15 +3,22 @@ import { IReviewToAssignComponentProps } from "./IReviewToAssignComponentProps";
 import styles from "./ReviewToAssignComponent.module.scss";
 import { BoldRegularText } from "../../../../../components/bold-regular-text/BoldRegularText";
 import { Link } from "react-router-dom";
+import { Box, useStyleConfig } from "@chakra-ui/react";
+import { StyledComponents } from "../../../../../common/enums/StyledComponents";
 
 export const ReviewToAssignComponent = (
   props: IReviewToAssignComponentProps
 ) => {
+  const secondaryStyles = useStyleConfig(StyledComponents.SecondaryComponent);
   const review = props.review;
 
   return (
-    <Link className={styles.reviewLink} to={"/reviewer"} state={{ review: review }}>
-      <div className={styles.review}>
+    <Link
+      className={styles.reviewLink}
+      to={"/reviewer"}
+      state={{ review: review }}
+    >
+      <Box className={styles.review} __css={secondaryStyles}>
         <BoldRegularText boldText={"Title: "} regularText={review.title} />
         <BoldRegularText boldText={"Project: "} regularText={review.project} />
         <BoldRegularText
@@ -22,7 +29,7 @@ export const ReviewToAssignComponent = (
           boldText={"Tags: "}
           regularText={String(review.tags)}
         />
-      </div>
+      </Box>
     </Link>
   );
 };
