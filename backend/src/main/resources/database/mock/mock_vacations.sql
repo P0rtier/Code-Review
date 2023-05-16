e646e99587031a11276dbc0990d8076d4dbf306e
P0rtier <patrykw595@gmail.com>
2023-05-16 18:57:55 +0200

    [PRES]: Vacations mockup data

---
+++ backend/src/main/resources/database/mock/mock_vacations.sql
@@ -0,0 +1,5 @@
+INSERT INTO vacations VALUES
+('1', '259102@student.pwr.edu.pl', '2023-05-22', '2023-05-31'),
+('2', '259105@student.pwr.edu.pl', '2023-05-26', '2023-06-05'),
+('3', '259086@student.pwr.edu.pl', '2023-05-16', '2023-05-22'),
+('4', '259065@student.pwr.edu.pl', '2023-06-01', '2023-06-10')
