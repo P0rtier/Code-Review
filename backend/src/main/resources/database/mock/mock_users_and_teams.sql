87d8a3184022f35f474d3ac15375963329bff787
kAleks12 <kacper4553@gmail.com>
2023-05-21 16:35:35 +0200

    Add leaderboard fix

---
+++ backend/src/main/resources/database/mock/mock_users_and_teams.sql
@@ -0,0 +1,16 @@
+INSERT INTO users VALUES
+('259105@student.pwr.edu.pl', 'Patryk Uzarowski'),
+('259102@student.pwr.edu.pl', 'Kamil Bauer'),
+('259086@student.pwr.edu.pl', 'Kacper Aleks'),
+('259065@student.pwr.edu.pl', 'Damian Gnieciak'),
+('259116@student.pwr.edu.pl', 'Igor Stalmach');
+
+
+INSERT INTO team_mappings VALUES
+(1,'259105@student.pwr.edu.pl', 'Code-Review Team'),
+(2, '259102@student.pwr.edu.pl', 'Code-Review Team'),
+(3, '259086@student.pwr.edu.pl', 'Code-Review Team'),
+(4, '259065@student.pwr.edu.pl', 'Code-Review Team'),
+(5, '259116@student.pwr.edu.pl', 'Code-Review Team'),
+(6,'259086@student.pwr.edu.pl', 'Test-team'),
+(7, '259102@student.pwr.edu.pl', 'Test-team');
