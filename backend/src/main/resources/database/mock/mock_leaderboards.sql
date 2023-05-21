6f70a015899c3082f90dff3670ae0949207f869b
P0rtier <patrykw595@gmail.com>
2023-05-21 12:47:03 +0200

    [PRES]: Add mock leaderboards

---
+++ backend/src/main/resources/database/mock/mock_leaderboards.sql
@@ -0,0 +1,6 @@
+INSERT INTO user_standings VALUES
+('1', '259105@student.pwr.edu.pl', '1', '20', '82823015-cb30-4f8f-9b08-b77627906d83'),
+('2', '259102@student.pwr.edu.pl', '2', '18', '82823015-cb30-4f8f-9b08-b77627906d83'),
+('3', '259086@student.pwr.edu.pl', '3', '16', '82823015-cb30-4f8f-9b08-b77627906d83'),
+('4', '259065@student.pwr.edu.pl', '4', '14', '82823015-cb30-4f8f-9b08-b77627906d83'),
+('5', '259116@student.pwr.edu.pl', '5', '12', '82823015-cb30-4f8f-9b08-b77627906d83');
